a24b415e4a43442312d90198e0a6e14870cd4cd9
Derek-94 <ghdtjrrl94@gmail.com>
2021-11-02 15:15:58 +0900

    feat(navigation): Update navigation component. (#1)

---
+++ src/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Navigation from './Navigation';
+
+const Main = () => {
+  return (
+    <>
+      <Navigation />
+      <h2>Hello world!</h2>
+    </>
+  );
+};
+
+export default Main;
