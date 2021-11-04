d86c6bddf34c1f54d6621573d69b96b4ed56fcfd
Derek-94 <ghdtjrrl94@gmail.com>
2021-11-04 22:58:42 +0900

    feat(data): Update dummy data modules. (#6)

---
+++ src/Article/container/ArticleListContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ArticleList from '../presenter/ArticleList';
+
+const ArticleListContainer = () => {
+  return <ArticleList />;
+};
+
+export default ArticleListContainer;
---
+++ src/Home/Home.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const Home: FunctionComponent = () => {
+  return <h1>Home!</h1>;
+};
+
+export default Home;
