ee559e7768e0fa4844fe5e455bfbb103ed1457b3
Derek-94 <ghdtjrrl94@gmail.com>
2021-11-05 11:10:32 +0900

    feat(article): Update article detail page. (#13)

--- src/Article/presenter/ArticleDetail.tsx
+++ src/Article/presenter/ArticleDetail.tsx
@@ -1,7 +1,41 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
+import { ArticleInfo } from '../../data/';
+
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
+import { Viewer } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 
 const ArticleDetail: FunctionComponent = () => {
-  return <h1>Article detail page</h1>;
+  const { id } = useParams<{ id: string }>();
+
+  const [title, setTitle] = useState<string>('');
+  const [content, setContent] = useState<string>('');
+
+  useEffect(() => {
+    const articles = localStorage.getItem('smileGate');
+    if (articles !== null) {
+      const targetArticle = JSON.parse(articles).filter(
+        (article: ArticleInfo) => article.id === id.substr(1),
+      );
+      setTitle(targetArticle[0].title);
+      setContent(targetArticle[0].content);
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>{title}</h1>
+      {content && (
+        <Viewer initialValue={content} plugins={[[codeSyntaxHighlight, { highlighter: Prism }]]} />
+      )}
+    </>
+  );
 };
 
 export default ArticleDetail;
